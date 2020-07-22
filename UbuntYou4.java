ca0c5eecd2a0df1bf4f67e25d953461bbfb6508b
963CHRISTOFAN963 <52283982+963CHRISTOFAN963@users.noreply.github.com>
2020-07-22 07:09:22 -0300

    Create UbuntYou4.java

---
+++ UbuntYou4.java
@@ -0,0 +1,14 @@
+package com.example.UbuntYou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UbuntYou4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_UbuntYou4);
+    }
+}
